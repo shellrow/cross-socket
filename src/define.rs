a77fe04e0d6b7b28c72b44cb1a6d6c1c446c0bec
shellrow <81893184+shellrow@users.noreply.github.com>
2023-05-13 16:23:01 +0900

    Update define.rs

--- src/define.rs
+++ src/define.rs
@@ -1,5 +1,5 @@
 // APP information
-pub const CRATE_UPDATE_DATE: &str = "2023-04-28";
+pub const CRATE_UPDATE_DATE: &str = "2023-05-12";
 pub const CRATE_REPOSITORY: &str = "https://github.com/shellrow/nesmap";
 
 // Setting
