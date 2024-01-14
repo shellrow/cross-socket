8e6db2048e5db4a214f267783cd1f4b836621baf
shellrow <81893184+shellrow@users.noreply.github.com>
2024-01-14 16:03:38 +0900

    Update Connection.vue

--- netpulsar-desktop/src/components/Connection.vue
+++ netpulsar-desktop/src/components/Connection.vue
@@ -4,6 +4,8 @@ import { invoke } from '@tauri-apps/api/tauri';
 //import { listen } from '@tauri-apps/api/event';
 import { KVItem } from '../types/common';
 import { ProcessSocketInfo } from '../types/np-types';
+import { setRoutine } from '../libnp/routine';
+import { WindowUtil } from '../libnp/window-util';
 import { DataTableRowSelectEvent } from 'primevue/datatable';
 
 const tableData = ref<ProcessSocketInfo[]>([]);
@@ -12,6 +14,7 @@ const selectedHost = ref<any>();
 const dialogVisible = ref(false);
 const isLoading = ref(false);
 const autoUpdate = ref(false);
+const windowUtil = new WindowUtil();
 
 const GetNetStat = async() => {
     isLoading.value = true;
@@ -20,6 +23,17 @@ const GetNetStat = async() => {
     tableData.value = result;
     isLoading.value = false;
 }
+
+const routine = setRoutine({
+  interval: 5000,
+  callback: () => { 
+        if (autoUpdate.value) {
+            GetNetStat(); 
+            console.log('updated');
+            console.log(windowUtil.windowSize);
+        }
+    }
+})
 
 const onRowSelect = (event: DataTableRowSelectEvent) => {
     dialogVisible.value = true;
@@ -69,11 +83,14 @@ const onRowUnselect = (_event: DataTableRowSelectEvent) => {
 }
 
 onMounted(() => {
+    windowUtil.mount();
     GetNetStat();
+    routine.start();
 });
 
 onUnmounted(() => {
-
+    windowUtil.unmount();
+    routine.stop();
 });
 
 </script>
@@ -99,16 +116,16 @@ onUnmounted(() => {
         <template #title> 
             <div class="flex justify-content-between">
                 <div class="flex">
-                    Active TCP connections and the TCP and UDP ports on which is listening. Click row for more detail.  
+                    Active TCP connections and the TCP and UDP ports on which is listening.  
                 </div>
                 <div class="flex">
                     <ToggleButton v-model="autoUpdate" onLabel="Auto" offLabel="Manual" onIcon="pi pi-play" offIcon="pi pi-pause" class="mr-2" />
-                    <Button type="button" icon="pi pi-refresh" outlined :loading="isLoading" @click="GetNetStat" />
+                    <Button type="button" icon="pi pi-refresh" outlined :loading="isLoading" @click="GetNetStat" :disabled="autoUpdate" />
                 </div>
             </div>
         </template>
         <template #content>
-            <DataTable :value="tableData" v-model:selection="selectedHost" :loading="isLoading" selectionMode="single" dataKey="index" @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" size="small" scrollable scrollHeight="70vh" tableStyle="min-width: 50rem">
+            <DataTable :value="tableData" v-model:selection="selectedHost" :loading="isLoading" selectionMode="single" dataKey="index" @rowSelect="onRowSelect" @rowUnselect="onRowUnselect" size="small" scrollable :scrollHeight="(windowUtil.windowSize.innerHeight-100).toString() + 'px'" tableStyle="min-width: 50rem">
                 <Column field="index" header="No" sortable></Column>
                 <Column field="socket_info.local_ip_addr" header="SRC IP Address" sortable></Column>
                 <!-- <Column field="local_hostname" header="SRC Host Name"></Column> -->
